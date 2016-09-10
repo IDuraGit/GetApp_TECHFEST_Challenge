31749a4b40792de110c44b2897200470b464e09c
Isaac <isaacdura@gmail.com>
2016-09-10 21:06:57 +0200

    Final GetApp version TechFEST Challenge @Test SendEmail

---
+++ src/test/java/test/TestSendEmail.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Isaac
+ */
+public class TestSendEmail {
+
+    
+    public TestSendEmail() {
+        //http://mundosica.com/enviando-correos-electronicos-desde-java-haciendo-clase-gmail-con-conexion-smpt-con-ssl/
+    }
+
+    @Test
+    public void test_setProperties() {
+
+    }
+
+    @Test
+    public void test_setAuthetication() {
+
+    }
+
+    @Test
+    public void test_sendEmail() {
+
+    }
+
+    @Test
+    public void test_GmailClass() {
+
+    }
+
+    @Test
+    public void test_TestAll() {
+
+    }
+
+}
