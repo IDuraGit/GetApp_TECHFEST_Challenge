14c76a2aa82c7ef246710eb39b18d89bf2379a76
Isaac <isaacdura@gmail.com>
2016-09-10 20:22:14 +0200

    Final GetApp version TechFEST Challenge

---
+++ src/test/java/test/TestActionTaken_by_Trigger.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import controller.ActionTaken_by_Trigger;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Isaac
+ */
+public class TestActionTaken_by_Trigger {
+
+    public TestActionTaken_by_Trigger() {
+
+        new ActionTaken_by_Trigger();
+    }
+
+    public void test_activaeMusic() {
+
+        //assertTrue();
+    }
+}
